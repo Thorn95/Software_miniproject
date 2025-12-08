c41f4ac290012c1caaa54dab03f254b204b189b5
a20anena <aneeqanadeem555@gmail.com>
2025-12-08 21:35:56 +0100

    Update filterLowExpressed.R

--- EnrichTool/R/filterLowExpressed.R
+++ EnrichTool/R/filterLowExpressed.R
@@ -1,3 +1,9 @@
+# This function removes genes that have very low counts.
+# Basically, it checks how many counts each gene has in total,
+# and if the count is too small, we drop that gene.
+# You can change the minimum count if you want (default is 10).
+
+
 filterLowExpressed <- function(count_data, min_count = 10) {
 
   # remove genes that don't have enough counts
