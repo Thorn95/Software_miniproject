280ffc2d03531a15f09eda61248d3da4621f9ddf
a20anena <aneeqanadeem555@gmail.com>
2025-12-12 19:56:19 +0100

    Update ora_res.R

--- EnrichTool/R/ora_res.R
+++ EnrichTool/R/ora_res.R
@@ -1,53 +1,64 @@
-#' ora_res
+#' ORA analysis (GO and KEGG)
 #'
-#' @description
-#' Performs over-representation analysis on set of genes.
+#' This function performs over-representation analysis for a list of
+#' significant genes. It runs GO enrichment and KEGG pathway enrichment.
 #'
-#' The "gene_data" is a dataframe where rownames are genes. Typically the
-#' output of a differentially expressed genes analysis. The rownames are used
-#' to convert to entrez id before enrichment.
+#' The input is usually the result from degAnalysis().
+#' Gene IDs are expected to be gene symbols (SYMBOL).
+#' For KEGG analysis, gene symbols are converted to ENTREZ IDs.
 #'
-#' @param gene_data Dataframe where rownames are genes.
-#' @param key_type Key type of the genes in the dataframe.
+#' @param gene_data Data frame with significant genes.
+#'   Genes should be in a column called "gene".
+#' @param key_type Type of gene ID used. Default is "SYMBOL".
 #'
-#' @return
-#' Ora results.
+#' @return A list with GO and KEGG enrichment results.
 #'
 #' @examples
-#' library(EnrichTool)
-#' ora_res(deg_res, key_type = "ENSEMBL")
+#' # res <- ora_res(deg_table)
+#' # res$GO
+#' # res$KEGG
 #'
 #' @export
-#'
-#'
-ora_res <- function(gene_data, key_type = "ENSEMBL"){
+ora_res <- function(gene_data, key_type = "SYMBOL") {
 
-  #Convert to entrez id
+  if (!requireNamespace("clusterProfiler", quietly = TRUE)) {
+    stop("clusterProfiler package is required.")
+  }
+  if (!requireNamespace("org.Hs.eg.db", quietly = TRUE)) {
+    stop("org.Hs.eg.db package is required.")
+  }
+
+  # get gene symbols from DEG table
+  genes <- gene_data$gene
+  genes <- unique(genes)
+
+  # convert gene symbols to ENTREZ IDs
   entrez_df <- clusterProfiler::bitr(
-                       geneID = rownames(gene_data),
-                       fromType = key_type,
-                       toType = "ENTREZID",
-                       OrgDb = org.Hs.eg.db::org.Hs.eg.db)
-
-  #Extract entrez vector
-  entrez_ids <- unique(entrez_df$ENTREZID)
-
-  #GO analysis
-  go_res <- clusterProfiler::enrichGO(
-    gene = entrez_ids,
-    keyType = "ENTREZID",
-    pvalueCutoff = 0.05,
-    pAdjustMethod = "BH",
-    OrgDb = org.Hs.eg.db::org.Hs.eg.db,
-    readable = TRUE
+    geneID   = genes,
+    fromType = key_type,
+    toType   = "ENTREZID",
+    OrgDb    = org.Hs.eg.db::org.Hs.eg.db
   )
 
-  #Kegg analysis
+  entrez_ids <- unique(entrez_df$ENTREZID)
+
+  # GO enrichment analysis
+  go_res <- clusterProfiler::enrichGO(
+    gene          = entrez_ids,
+    OrgDb         = org.Hs.eg.db::org.Hs.eg.db,
+    keyType       = "ENTREZID",
+    pvalueCutoff  = 0.05,
+    pAdjustMethod = "BH",
+    readable      = TRUE
+  )
+
+  # KEGG pathway enrichment
   kegg_res <- clusterProfiler::enrichKEGG(
-    gene = entrez_ids,
-    organism = "hsa",
+    gene         = entrez_ids,
+    organism     = "hsa",
     pvalueCutoff = 0.05
   )
 
-  return(list(GO = go_res, KEGG = kegg_res))
+  list(GO = go_res, KEGG = kegg_res)
 }
+
